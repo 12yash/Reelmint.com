0b3e36c4d99e0831afb1e88f297f23bbfbdd5ab2
12yash <70858067+12yash@users.noreply.github.com>
2022-06-19 22:12:37 +0530

    hhy

---
+++ COMPILATION/script.js
@@ -0,0 +1,41 @@
+var typeText = document.querySelector(".typeText")
+var textToBeTyped = "a software engineer"
+var textToBeTypedArr = ["GET the best ASSETS for Concept art and CG!", "Clear explanation", "Learn advance skills including facial anim.", "Join and Subscribe our patreon now."]
+var index = 0, isAdding = true, textToBeTypedIndex = 0
+
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of the text to be typed using index.
+    typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTypedArr[textToBeTypedIndex].length) {
+        // no more text to add
+        isAdding = false
+        //break: wait 2s before playing again
+        setTimeout(function () {
+          playAnim()
+        }, 2000)
+        return
+      } else {
+        // increment index by 1
+        index++
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        // no more text to remove
+        isAdding = true
+        //switch to next text in text array
+        textToBeTypedIndex = (textToBeTypedIndex + 1) % textToBeTypedArr.length
+      } else {
+        // decrement index by 1
+        index--
+      }
+    }
+    // call itself
+    playAnim()
+  }, isAdding ? 120 : 60)
+}
+// start animation
+playAnim()
